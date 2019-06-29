3c77fc2fb8d008ecd23327047032cc1899e7ae6a
PepperKick <abhishrant.m@gmail.com>
2019-06-29 12:57:55 +0530

    Added support for cap percentage

--- vue/components/Hud/ControlPoints.vue
+++ vue/components/Hud/ControlPoints.vue
@@ -3,6 +3,7 @@
         <div id='points-container' v-if="round.gameType === '5CP'">
             <div class='point-info' v-for='i in 5' :class='getPointClasses(i - 1)' :key='i'>
                 <span class='num-capping' v-if='isCapping(i - 1)'>x{{ getNumCapping(i - 1) }}</span>
+                <div class='cap-progress' :class='getCaputreProgressClass(i - 1)' :style='getCaputreProgressStyle(i - 1)' v-if='getCappingTeam(i - 1) > 1'></div>
             </div>
         </div>
     </div>
@@ -13,30 +14,33 @@ export default {
     props: [ 'round' ],
     methods: {
         getPointClasses(i) {
-            const lazyperc = this.round.cap2.percentage
-            const teamtime = this.round.cap2.teamTime
-            const caplength = teamtime
-            const capleft = lazyperc * teamtime
-            const eltime = caplength - capleft
-
-            // console.log(lazyperc, teamtime, caplength, capleft, eltime)
-
-            if (caplength <= 0) console.log(0)
-            else if (eltime > caplength) console.log(1)
-            else console.log(eltime / caplength)
-
             return {
                 'point-owned-none': this.round[`cap${i}`].cappedTeam == 0,
                 'point-owned-blue': this.round[`cap${i}`].cappedTeam == 2,
                 'point-owned-red': this.round[`cap${i}`].cappedTeam == 3,
-                'point-capping-none': this.round[`cap${i}`].cappingTeam == 0,
-                'point-capping-blue': this.round[`cap${i}`].cappingTeam == 3,
-                'point-capping-red': this.round[`cap${i}`].cappingTeam == 2,
                 'point-locked': this.round[`cap${i}`].locked == 1,
                 'point-unlocked': this.round[`cap${i}`].locked == 0,
                 'point-blocked': this.round[`cap${i}`].locked == 1,
                 'point-unblocked': this.round[`cap${i}`].locked == 0
             }
+        },
+
+        getCaputreProgressStyle (i) {
+            let percentage = parseInt(parseFloat(this.round[`cap${i}`].percentage) * 100)
+            percentage = percentage < 0 ? 0 : percentage > 100 ? 100 : percentage;
+
+            console.log(percentage)
+
+            return {
+                'width': `${percentage}%`
+            }
+        },
+
+        getCaputreProgressClass (i) {
+            return {
+                'cap-progress-red': this.getCappingTeam(i) == 2,
+                'cap-progress-blue': this.getCappingTeam(i) == 3,
+            }            
         },
 
         getNumCapping(i) {
@@ -47,6 +51,10 @@ export default {
 
         isCapping(i) {
             return this.round[`cap${i}`].cappingTeam != 0 && this.round[`cap${i}`].playersOnCap != 0
+        },
+
+        getCappingTeam(i) {
+            return this.round[`cap${i}`].cappingTeam
         }
     }
 }
@@ -72,17 +80,21 @@ export default {
         flex-direction: row;
 
         .point-info {
+            position: relative;
             height: @point-height;
             width: @point-height;
             transition: 0.3s;
             display: flex;
             margin: auto;
+            background: rgba(0, 0, 0, 0.5);
 
             .num-capping {
+                position: absolute;
                 color: #fff;
                 font-size: 32px;
                 font-weight: 900;
-                margin: auto;
+                top: 4px; left: 8px;
+                z-index: 3;
             }
         }
     }
@@ -99,16 +111,22 @@ export default {
         border: @point-border-width solid @blue-team-color;
     }
 
-    .point-capping-none {
-        background: rgba(0, 0, 0, 0.5);
+    .cap-progress {
+        width: 0;
+        height: 100%;
+        z-index: 1;
     }
 
-    .point-capping-blue {
-        background: rgba(red(@blue-team-color), green(@blue-team-color), blue(@blue-team-color), 0.5);
+    .cap-progress-blue {
+        margin: auto;
+        margin-left: 0;
+        background: rgba(red(@blue-team-color), green(@blue-team-color), blue(@blue-team-color), 0.8);
     }
 
-    .point-capping-red {
-        background: rgba(red(@red-team-color), green(@red-team-color), blue(@red-team-color), 0.5);
+    .cap-progress-red {
+        margin: auto;
+        margin-right: 0;
+        background: rgba(red(@red-team-color), green(@red-team-color), blue(@red-team-color), 0.8);
     }
 }
 </style>
