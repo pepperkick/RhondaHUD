e16614af89e12826777a3684034c34cc5911a8cb
PepperKick <abhishrant.m@gmail.com>
2019-07-14 11:17:41 +0530

    Removed socket from server

--- server/main.js
+++ server/main.js
@@ -7,22 +7,10 @@ const io = require('socket.io')(config.get('server.socket_port'))
 const Store = require('./store.js')()
 const Api = require('./api.js')(io)
 
-const GSI_PIPE_PATH = '\\\\.\\pipe\\tf2-gsi'
 const log = debug('app:main')
 
 let info = {}
 
-const server = net.createServer((stream) => {
-    stream.on('data', async (data) => {    
-        // log(data.toString());
-        const values = data.toString()
-        info = JSON5.parse(values)
-
-        // log(info.event);
-
-        io.sockets.emit('data', info)
-    });
-});
 
 io.on('connect', async client => {
     const config = await Store.Config.Get()
@@ -30,8 +18,4 @@ io.on('connect', async client => {
     client.emit('config', config)
 
     log('Client connected')
-});
-
-server.listen(GSI_PIPE_PATH, () => {
-    log('GSI Pipe Started!')
 });
